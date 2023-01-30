32a14b631dbeda6fe4de5f4331982b751786fa70
Sayed Ali Husain <s0ali@outlook.com>
2023-01-30 14:49:12 +0300

    -Add the timer -Counter is in consol

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        if timer.isValid {
+            timer.invalidate()
+        }
+        
+        let hardness = sender.currentTitle!
+        print("\(eggTimes[hardness]!) minuts")
+        timer = startTimer(time: eggTimes[hardness]!)
+        
+    }
+    
+    func startTimer(time: Int) -> Timer {
+        var counter = time
+        
+        return Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if counter > 0 {
+                print("\(counter) seconds left")
+                counter -= 1
+            } else {
+                Timer.invalidate()
+                print("DONE!!!")
+            }
+        }
+    }
+    
 }
-
